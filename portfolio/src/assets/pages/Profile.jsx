112a98c1e96ffae9a5c1f4eae4ea6757fa59017c
Kathircoder <bkathiravan01@gmail.com>
2024-09-20 16:29:05 +0530

    updated

--- portfolio/src/assets/pages/Profile.jsx
+++ portfolio/src/assets/pages/Profile.jsx
@@ -1,7 +1,22 @@
 const Profile = () => {
     return (
         <>
-            <div>Profile</div>
+            <div className="text-black font-bold text-3xl p-20"> 
+            <div > Hi Iam kathiravan <span className="text-red-600 font-bold text-3xl">B</span> 
+            </div>
+            <div className="text-red-600 font-bold text-3xl">Software Developer</div>
+            <div className="py-8 w-1/2 text-black font-bold text-lg "> leverages cutting-edge information technology to advance biopharmaceutical research and development. By harnessing data analytics, machine learning, and bioinformatics, Portio Bio enhances the discovery and optimization of innovative therapeutics.</div>
+            </div>
+            <div className="flex flex-row justify-center text-center p-5 gap-7 w-full">
+          <img src="https://res.cloudinary.com/dve8r06ul/image/upload/v1726569019/atom_wcytoz.png" className="h-10 w-10 "/>
+          <img src="https://res.cloudinary.com/dve8r06ul/image/upload/v1726568558/developer_g80rja.png" className="h-10 w-10 "/>
+          <img src="https://res.cloudinary.com/dve8r06ul/image/upload/v1726739795/html_a6djri.png" className="h-10 w-10 "/>
+          <img src="https://res.cloudinary.com/dve8r06ul/image/upload/v1726739926/cascading-style-sheets_m3eb94.png" className="h-10 w-10 "/>
+          <img src="https://res.cloudinary.com/dve8r06ul/image/upload/v1726740319/java_xxqlqe.png" className="h-10 w-10 "/>
+          <img src="https://res.cloudinary.com/dve8r06ul/image/upload/v1726741241/python_tdjbi6.png" className="h-10 w-10 "/>
+          <img src="https://res.cloudinary.com/dve8r06ul/image/upload/v1726742577/sql-server_l3olh9.png" className="h-10 w-10 "/>
+
+        </div>
         </>
     )
 }
